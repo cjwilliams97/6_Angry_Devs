f845890aca447edae52eaa80fe86e10cbc42c9af
Sheldon-Lockie <lock4276@vandals.uidaho.edu>
2019-01-30 08:37:46 -0800

    Update Player_Control.cs
    Added inputs, max speed, responsivness/sensitivity values

--- Unity Project/Assets/SL/Scripts/Player_Control.cs
+++ Unity Project/Assets/SL/Scripts/Player_Control.cs
@@ -6,13 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class Player_Control: MonoBehaviour
 {
-    public float speed;
+    public float Max_Speed;
+    public float Foward_Speed;
+    public float Brake_Speed;
+    public float Responsiveness;
+
+    public float Turn_Sensitivity;
+    
     private Rigidbody rigid;
     void start(){
         rigid =  GetComponent<Rigidbody>();
     }
     void Update()
     {
+            float xVel =  transofrm.InverseTransformDirection(rigid.velocity).x;
+            if(Input.GetKey("W")){
+                if(xVel < Max_Speed){
+                    rigid.AddForce(Foward_Speed * Responsiveness);
+                }
+            }
+            if(Input.GetKey("S")){
+                rigid.AddForce(- (Brake_Speed* Responsiveness));
+            }
+            if(Input.GetKey("A")){
+                rigid.AddForce(0.0f,Turn_Sensitivity * Responsiveness);
+            }
+            if(Input.GetKey("D")){
+                rigid.AddForce(0.0f,-(Turn_Sensitivity * Responsiveness));
+            }
     
     }
 }
