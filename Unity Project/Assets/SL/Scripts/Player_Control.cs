f212fa3e0a88074e0bbc29867dcc1ee7ca5fd0f9
Sheldon-Lockie <lock4276@vandals.uidaho.edu>
2019-01-31 09:58:19 -0800

    Update Player_Control.cs

--- Unity Project/Assets/SL/Scripts/Player_Control.cs
+++ Unity Project/Assets/SL/Scripts/Player_Control.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class Player_Control : MonoBehaviour
-{
+public class Player_Control : MonoBehaviour {
     public float Max_Speed;
     public float Foward_Speed;
     public float Brake_Speed;
