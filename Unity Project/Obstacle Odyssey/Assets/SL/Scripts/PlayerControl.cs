b6fae510312775b5501d5d962427d30212cfa23a
cawndog6 <47013360+cawndog6@users.noreply.github.com>
2019-02-07 23:25:18 -0800

    more fixing

---
+++ Unity Project/Obstacle Odyssey/Assets/SL/Scripts/PlayerControl.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerControl : MonoBehaviour {
+    public float Max_Speed;
+    public float Foward_Speed;
+    public float Brake_Speed;
+    public float Responsiveness;
+
+    public float Turn_Speed;
+    //scale from 0-1 
+
+    public float Pitch_Force;
+    
+    public float Brake_Force;
+
+    public float Accel_Force;
+    private Rigidbody rigid;
+    void start(){
+        rigid =  GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+            float xVel =  transofrm.InverseTransformDirection(rigid.velocity).x;
+            if(Input.GetKey(KeyCode.W)){
+                if(xVel < Max_Speed){
+                    rigid.AddForce(Foward_Speed * Responsiveness,0.0f,0.0f);
+                    rigid.AddTorque(0.0f,Accel_Force,0.0f);
+                }
+            }
+            if(Input.GetKey("S")){
+                rigid.AddForce(- (Brake_Speed* Responsiveness,0.0f,0.0f));
+                rigid.AddTorque(0.0f,-(Brake_Force),0.0f);
+            }
+            if(Input.GetKey("A")){
+                rigid.AddForce(0.0f,Turn_Speed * Responsiveness,0.0f);
+                rigid.AddTorque(Pitch_Force,0.0f,0.0f);
+            }
+            if(Input.GetKey("D")){
+                rigid.AddForce(0.0f,-(Turn_Speed * Responsiveness,0.0f));
+                rigid.AddTorque(-(Pitch_Force),0.0f,0.0f);
+            }
+    
+    }
+}
