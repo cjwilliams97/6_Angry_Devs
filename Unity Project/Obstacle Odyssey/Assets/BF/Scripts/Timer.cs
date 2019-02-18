e12f1071b2626fa33fa40b8af69c6e9803c24abb
Brandon Foss <46940941+FossBrandon@users.noreply.github.com>
2019-02-17 22:27:18 -0800

    added game functions

--- Unity Project/Obstacle Odyssey/Assets/BF/Scripts/Timer.cs
+++ Unity Project/Obstacle Odyssey/Assets/BF/Scripts/Timer.cs
@@ -7,6 +7,7 @@ public class Timer : MonoBehaviour
 {
     public Text timerText;
     private float startTime;
+    private bool finnished = false;
 
     // Start is called before the first frame update
     void Start()
@@ -17,11 +18,22 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(finnished)
+        {
+            return;
+        }
+
         float t = Time.time - startTime;
 
         string minutes = ((int)t / 60).ToString();
         string seconds = (t % 60).ToString("f2");
 
         timerText.text = minutes + ":" + seconds;
+    }
+
+    public void Finnish()
+    {
+        finnished = true;
+        timerText.color = Color.yellow; 
     }
 }
