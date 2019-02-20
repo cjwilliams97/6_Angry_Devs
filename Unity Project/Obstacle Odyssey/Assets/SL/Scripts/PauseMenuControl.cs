d57ace46cb673661dc55ddd4dd91c7ef7924b27c
Sheldon-Lockie <Sherlockssl@gmail.com>
2019-02-20 12:06:32 -0800

    Added Restart Option
    Not Implemented  fully yet, needs timer reset script

--- Unity Project/Obstacle Odyssey/Assets/SL/Scripts/PauseMenuControl.cs
+++ Unity Project/Obstacle Odyssey/Assets/SL/Scripts/PauseMenuControl.cs
@@ -6,17 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenuControl : MonoBehaviour
 {
-    public Button Resume_btn, Setting_Btn, Exit_btn;
+    public Button Resume_btn, Reset_btn, Exit_btn;
+    public Rigidbody rigid;
 
     void Start()
     {
         Resume_btn.onClick.AddListener(Resume_Clicked);
+        Reset_btn.onClick.AddListener(Restart_Clicked);
         Exit_btn.onClick.AddListener(Exit_Clicked);
     }
 
     void Resume_Clicked()
     {
         
+    }
+    void Restart_Clicked()
+    {
+        //rigid.GetComponent<Timer>().Start();
+        SceneManager.LoadScene("GameScene", LoadSceneMode.Single);
     }
     void Exit_Clicked()
     {
