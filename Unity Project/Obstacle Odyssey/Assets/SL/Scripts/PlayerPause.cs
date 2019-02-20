8c39b326b6ffd9e03ad7acd31511cf03d2a3ab64
Sheldon-Lockie <lock4276@vandals.uidaho.edu>
2019-02-20 09:05:41 -0800

    Added Pause Functionality

---
+++ Unity Project/Obstacle Odyssey/Assets/SL/Scripts/PlayerPause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPause : MonoBehaviour
+{
+    private KeyCode Escape = KeyCode.Escape;
+    private Rigidbody rigid;
+    public bool Paused = false;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void LateUpdate()
+    {
+        if (Input.GetKeyDown(Escape))
+        {
+            if (Paused == false)
+            {
+                Paused = true;
+                rigid.GetComponent<Timer>().PausedGame();
+                Time.timeScale = 0.0f;
+                Debug.Log("Escape Initiated");
+                
+            }
+            else if(Paused == true)
+            {
+                Paused = false;
+                rigid.GetComponent<Timer>().ResumedGame();
+                Time.timeScale = 1.0f;
+                Debug.Log("Escape Closed");
+            }
+        }
+    }
+}
