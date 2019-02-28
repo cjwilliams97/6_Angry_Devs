230c7e5d777b6e9ff91d7b5a46117768d620fdc8
Code-Breacher <43709796+Code-Breacher@users.noreply.github.com>
2019-02-27 17:15:08 -0800

    Add files via upload

---
+++ Unity Project/Obstacle Odyssey/Assets/JM/Scripts/Barrel_Sway.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Barrel_Sway : MonoBehaviour
+{
+    public float delta_y = 1.5f;
+    public float delta = 3f;
+    public float speed = 2.0f;
+    private Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 v = startPos;
+        v.z += delta * Mathf.Sin(Time.time * (speed/2));
+        v.y += delta_y * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
