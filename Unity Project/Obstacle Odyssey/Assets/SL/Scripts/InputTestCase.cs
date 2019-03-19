3775f2f2ee4865a39f2aa32c52f1fd640f4db0b6
Sheldon-Lockie <lock4276@vandals.uidaho.edu>
2019-03-19 14:58:44 -0700

    More Editing

---
+++ Unity Project/Obstacle Odyssey/Assets/SL/Scripts/InputTestCase.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputTestCase : MonoBehaviour
+{
+    public Text TimeBetween;
+    public float Time = 1.00;
+    public float Increment = .05;
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeBetween.text = "0.000";   
+    }
+
+    void FixedUpdate()
+    {
+        float i;
+        for (i = Time; i > 0; i-Increment)
+        {
+            string temp = Time.ToString();
+            TimeBetween.text = temp;
+            Time - Increment;
+        }
+    }
+}
---
+++ Unity Project/Obstacle Odyssey/Assets/SL/Scripts/VelocityUI.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VelocityUI : MonoBehaviour
+{
+    public Rigidbody rigid;
+    public Text VelocityValue;
+    public float Velocity;
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+        VelocityValue.text = "000.00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Velocity = rigid.velocity.magnitude;
+        string temp = Velocity.ToString();
+        VelocityValue.text = temp;
+
+    }
+    public float CurrentVelocity()
+    {
+        return Velocity;
+    }
+}
