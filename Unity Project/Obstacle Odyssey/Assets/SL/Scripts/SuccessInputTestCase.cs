7d21ea60143ac49030c89bf6cef27b1abe678aed
Sheldon-Lockie <lock4276@vandals.uidaho.edu>
2019-03-20 11:01:22 -0700

    More Stuff

---
+++ Unity Project/Obstacle Odyssey/Assets/SL/Scripts/SuccessInputTestCase.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SuccessInputTestCase : MonoBehaviour
+{
+    public Text TimeBetweenInputs;
+    public Rigidbody rigid;
+
+    void Start()
+    {
+        rigid.GetComponent<Rigidbody>();
+        float safeVal = rigid.GetComponent<FailInputTestCase>().MaxInputValue();
+        string temp = safeVal.ToString();
+        TimeBetweenInputs.text = temp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
