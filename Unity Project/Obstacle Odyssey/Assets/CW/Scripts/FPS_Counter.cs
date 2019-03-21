052fe51d9370981d685a4e9264c86016f5385a04
cawndog6 <47013360+cawndog6@users.noreply.github.com>
2019-03-20 18:13:32 -0700

    Connor Stress Test Scence

---
+++ Unity Project/Obstacle Odyssey/Assets/CW/Scripts/FPS_Counter.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS_Counter : MonoBehaviour
+{
+    public static int avgFrameRate;
+    public Text display_Text;
+
+    public void Update()
+    {
+        float current = 60;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = avgFrameRate.ToString() + " FPS";
+    }
+}
