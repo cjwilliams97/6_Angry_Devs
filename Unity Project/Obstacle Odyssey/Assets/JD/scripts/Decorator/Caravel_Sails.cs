790da4ca1ae06f1d5a982d174bc6f24220e63728
Joshua Dempsey <demp5996@vandals.uidaho.edu>
2019-03-25 22:30:15 -0700

    updated decorator scripts
    added samll translations (probs not final)

--- Unity Project/Obstacle Odyssey/Assets/JD/scripts/Decorator/Caravel_Sails.cs
+++ Unity Project/Obstacle Odyssey/Assets/JD/scripts/Decorator/Caravel_Sails.cs
@@ -16,17 +16,13 @@ public class Caravel_Sails : ShipDecorator
 
     private class Sails : MonoBehaviour
     {
-        public void AddSails()
+         public void AddSails()
         {
             Debug.Log("Instantiating caravel attribute");
             Attribute = Instantiate(Resources.Load("JD/Caravel/caravel_sails", typeof(GameObject)), PARENT.transform.position, PARENT.transform.rotation) as GameObject;
             Attribute.transform.localScale = PARENT.transform.lossyScale;
             Attribute.transform.SetParent(PARENT.transform);
-            Attribute.transform.Translate(.00223f, .00064f, .00286f);
-
-
-
-
+            Attribute.transform.localPosition += new Vector3(0.001f, 0.001f, 0.004f);
             return;
         }
     }
