5b1ddc074bc6d53dbfa36bf0e3230d8fa8c6467f
Joshua Dempsey <demp5996@vandals.uidaho.edu>
2019-04-14 13:47:39 -0700

    Added level
    fixed generatesmoke scripst
    made prefab
    started cave level

---
+++ Unity Project/Obstacle Odyssey/Assets/src/JD/Scripts/CaravelLights.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaravelLights : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject PARENT;
+    GameObject FrontLantern;
+    GameObject BacklanternL;
+    GameObject BackLanternR;
+    void Awake()
+    {
+        PARENT = GameObject.Find("Caravel");
+        FrontLantern = GameObject.Find("FrontLantern");
+        BacklanternL = GameObject.Find("BacklanternL");
+        BackLanternR = GameObject.Find("BackLanternR");
+
+        FrontLantern.transform.position = PARENT.transform.position;
+        FrontLantern.transform.localScale = PARENT.transform.lossyScale;
+        FrontLantern.transform.SetParent(PARENT.transform);
+        FrontLantern.transform.localPosition += new Vector3(0.065f, 0.001f, 0.02f);
+
+        BacklanternL.transform.position = PARENT.transform.position;
+        BacklanternL.transform.localScale = PARENT.transform.lossyScale;
+        BacklanternL.transform.SetParent(PARENT.transform);
+        BacklanternL.transform.localPosition += new Vector3(-0.050f, -0.00777f, 0.022f);
+
+        BackLanternR.transform.position = PARENT.transform.position;
+        BackLanternR.transform.localScale = PARENT.transform.lossyScale;
+        BackLanternR.transform.SetParent(PARENT.transform);
+        BackLanternR.transform.localPosition += new Vector3(-0.050f, 0.01f, 0.022f);
+    }  
+}
