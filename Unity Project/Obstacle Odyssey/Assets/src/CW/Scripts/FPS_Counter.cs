07490ebbf2631a689740a75ec71ccae12f8357e4
cawndog6 <47013360+cawndog6@users.noreply.github.com>
2019-04-14 18:10:57 -0700

    Added FPS Counter to game scenes

---
+++ Unity Project/Obstacle Odyssey/Assets/src/CW/Scripts/FPS_Counter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPS_Counter : MonoBehaviour
+{
+    public static int avgFrameRate;
+    public Text display_Text;
+    public GameObject FPS_Counter_obj;
+    private bool IsActive;
+
+    public void Start()
+    {
+        IsActive = false;
+        FPS_Counter_obj.SetActive(false);
+    }
+    public void Update()
+    {
+        //FPS Counter code (stolen)
+        float current = 60;
+        current = (int)(1f / Time.unscaledDeltaTime);
+        avgFrameRate = (int)current;
+        display_Text.text = avgFrameRate.ToString() + " FPS";
+
+        //show or hide the FPS counter (mine)
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (IsActive == false)
+            {
+                IsActive = true;
+                FPS_Counter_obj.SetActive(true);
+            }
+            else
+            {
+                IsActive = false;
+                FPS_Counter_obj.SetActive(false);
+            }
+        }
+    }
+}
