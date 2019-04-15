1eeade21c410f52ca29de8c4ea1d4e17b964e55a
Brandon Foss <46940941+FossBrandon@users.noreply.github.com>
2019-04-15 00:17:53 -0700

    Updated Gantt and personal scripts

--- Unity Project/Obstacle Odyssey/Assets/src/BF/Scripts/ShipReturn.cs
+++ Unity Project/Obstacle Odyssey/Assets/src/BF/Scripts/ShipReturn.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+﻿/* Brandon Foss
+ * This script will use a state pattern to change the caravel object
+ * based on its current state. It is used to return the ship to our standard 
+ * starting position if the physics send it elsewhere when loading the level.
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -14,6 +20,7 @@ namespace StatePattern
             counter = 20;
         }
 
+        // runs update 20 times and continues to update boat based on state
         void Update()
         {
             //Debug.Log("1 - Made it to Update number: " + counter);
@@ -30,7 +37,7 @@ namespace StatePattern
             }
         }
 
-       
+        // lists different states the boat can be in
         protected enum BoatPosition
         {
             Standard,
@@ -39,6 +46,7 @@ namespace StatePattern
 
         BoatPosition initialLocationMode = BoatPosition.Offset; // sets boat to offset state initially
 
+        // changes the boats state based on position of it
         public virtual void UpdateBoat()
         {
             //Debug.Log("1 - Made it to UpdateBoat");
@@ -48,6 +56,7 @@ namespace StatePattern
 
             switch (initialLocationMode)
             {
+                // indicates boat is still offset and keeps state that way
                 case BoatPosition.Offset:
                     if (valueX >= 2 || valueY >= 5.5 || valueZ >= 2 || valueX <= -2
                         || valueY <= 3.5 || valueZ <= -2)
@@ -56,6 +65,7 @@ namespace StatePattern
                         initialLocationMode = BoatPosition.Offset; 
                     }
 
+                    // indicates boat is now standard position and switches state from offset to standard 
                     else if (valueX <= 2 && valueY <= 5.5 && valueZ <= 2 && valueX >= -2
                         && valueY >= 3.5 && valueZ >= -2)
                     {
@@ -64,7 +74,8 @@ namespace StatePattern
                     }
 
                     break;
-
+    
+                // indicates boat is still in standard position and maintains state
                 case BoatPosition.Standard:
                     if (valueX <= 2 && valueY <= 5.5 && valueZ <= 2 && valueX >= -2
                         && valueY >= 3.5 && valueZ >= -2)
@@ -73,23 +84,35 @@ namespace StatePattern
                         initialLocationMode = BoatPosition.Standard;
                     }
 
+                    // indiactes boat is offset and switches state from standard to offset
+                    else if (valueX >= 2 || valueY >= 5.5 || valueZ >= 2 || valueX <= -2
+                        || valueY <= 3.5 || valueZ <= -2)
+                    {
+                        //Debug.Log("1 - Made it to case BoatPosition.Offset first if");
+                        initialLocationMode = BoatPosition.Offset;
+                    }
+
                     break;
             }
 
+            // performs action based off of internal state
             DoAction(initialLocationMode);
 
         }
 
+        // will perform action based on state 
         protected void DoAction(BoatPosition location)
         {
             //Debug.Log("2 - Made it to DoAction");
             switch (location)
             {
+                // if standard do nothing
                 case BoatPosition.Standard:
                     //Debug.Log("2 - Made it to case BoatPosition.Standard:");
                     // do nothing
                     break;
 
+                // if offset, move boat properly to standard
                 case BoatPosition.Offset:
                     // move ship back to original
                     //Debug.Log("2 - Made it to case BoatPosition.offset: ");
