66ea9d300740f16ce4e52c14e2022fcbdab7e048
Sheldon-Lockie <lock4276@vandals.uidaho.edu>
2019-04-24 08:59:12 -0700

    Added TimedFireMethod+ SIngle fire Method

--- Unity Project/Obstacle Odyssey/Assets/tst/SL/Scripts/CannonFire.cs
+++ Unity Project/Obstacle Odyssey/Assets/tst/SL/Scripts/CannonFire.cs
@@ -9,7 +9,8 @@ public class CannonFire : MonoBehaviour
 
     private Vector3 CannonFirePointLocation;
     public GameObject CannonFirePoint;
-    public Material CannonballMat;
+    public Texture CannonballMat;
+    private IEnumerator coroutine;
     private Rigidbody rigid;
     private GameObject Ball;
     public float Power;
@@ -17,6 +18,7 @@ public class CannonFire : MonoBehaviour
     public GameObject ExplosionObj;
     private GameObject Explosion;
     private bool IsInFiringCooldown;
+    
 
 
     void Start()
@@ -24,6 +26,8 @@ public class CannonFire : MonoBehaviour
         Power = 1000f;
         FireTime = 1.0f;
         IsInFiringCooldown = false;
+        KeepFiring = true;
+
     }
 
     // Update is called once per frame
@@ -34,7 +38,7 @@ public class CannonFire : MonoBehaviour
             if (Input.GetKeyDown(Fire))
             {
 
-                FireCannon(Power);
+                FireCannonTimed(Power,FireTime);
 
 
             }
@@ -55,6 +59,7 @@ public class CannonFire : MonoBehaviour
         Ball.transform.position = CannonFirePoint.transform.position;
         Ball.transform.eulerAngles = CannonFirePoint.transform.eulerAngles;
         Ball.AddComponent<Rigidbody>();
+        Ball.GetComponent<Renderer>().material.mainTexture = CannonballMat;
         rigid = Ball.GetComponent<Rigidbody>();
         rigid.AddRelativeForce(Power, 0.0f, 0.0f);
         Explosion = (GameObject)Instantiate(ExplosionObj);
@@ -67,6 +72,47 @@ public class CannonFire : MonoBehaviour
         Destroy(Ball, 15.0f);
         
         
+    }
+    /*
+     * Requires a float for power of shot I Recommend 1000.0f for a medium shot
+     * Also Requires a float time, which is the time between shots in Seconds
+     * Calling this once will start
+     * Destroys explosion gameobject after 5s
+     * Destorys Ball after 20s
+     * 
+     */
+    public void FireCannonTimed(float power, float time)
+    {   
+        coroutine = FireTimer(time);
+        IsInFiringCooldown = true;
+        Debug.Log("Firing cooldown is.. " + IsInFiringCooldown);
+        Ball = (GameObject)Instantiate(CannonBall);
+        Ball.transform.parent = CannonFirePoint.transform;
+        Ball.transform.position = CannonFirePoint.transform.position;
+        Ball.transform.eulerAngles = CannonFirePoint.transform.eulerAngles;
+        Ball.AddComponent<Rigidbody>();
+        Ball.GetComponent<Renderer>().material.mainTexture = CannonballMat;
+        rigid = Ball.GetComponent<Rigidbody>();
+        rigid.AddRelativeForce(Power, 0.0f, 0.0f);
+        Explosion = (GameObject)Instantiate(ExplosionObj);
+        Explosion.transform.parent = CannonFirePoint.transform;
+        Explosion.transform.position = CannonFirePoint.transform.position;
+        Explosion.transform.eulerAngles = CannonFirePoint.transform.eulerAngles;
+        Explosion.transform.Rotate(0.0f, 0.0f, 90.0f);
+        Explosion.transform.localScale = new Vector3(.001f, .001f, .001f);
+        StartCoroutine(coroutine);
+        Destroy(Explosion, 5.0f);
+        Destroy(Ball, 15.0f);
+        
+        
+    }
+    private IEnumerator FireTimer(float time)
+    {
+        Debug.Log("Fire Time is .." + time);
+        yield return new WaitForSeconds(time);
+        IsInFiringCooldown = false;
+        Debug.Log("Firing cooldown is.. " + IsInFiringCooldown);
+        FireCannonTimed(Power, FireTime);
     }
 
 }
