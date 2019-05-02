c08c37fef1a50945dc6af1e3f2945ab622f45e24
Brandon Foss <46940941+FossBrandon@users.noreply.github.com>
2019-05-01 20:05:12 -0700

    updated script

---
+++ Unity Project/Obstacle Odyssey/Assets/src/BF/Scripts/Collisions/CannonBallCollision.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallCollision : Collision
+{
+  
+    void Start()
+    {
+        base.DoInitialization(); // calls base class initialization
+        damage = 75; // changes damage value after initialization
+    }
+
+    // ovverides method to form to rock collisions
+    public override void OnTriggerEnter(Collider other)
+    {
+        if (active)
+        {
+            GameObject.Find("Scripts").SendMessage("HealthChangeDamage", damage); // calls damage script
+            sfxHandler.PlayAudio("explosion echo"); // calls audio script to play rock sounds
+            base.StartCoroutine("DisableScript"); // disables script for 3 seconds to avoid rapid collisions
+        }
+    }
+}
